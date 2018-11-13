5e9da8328d29c058704b12f718f82c2a64945e78
Vlad <vturshyn@gmail.com>
2018-11-13 20:17:47 +0200

    cur route for test

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -75,7 +75,7 @@ router.get('/cur',VerifyToken, function(req, res){
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
-      });
+    });
 });
 
 module.exports = router;
