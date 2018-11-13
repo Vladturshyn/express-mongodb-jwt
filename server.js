8f55bcd96ee278e31672e1d0c64cbf31b3ff778d
Vlad <vturshyn@gmail.com>
2018-11-13 16:43:07 +0200

    mongodb mongoose mlab & express server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// BD config
+const db = require('./config/keys').mongoURI;
+// Connect to MongoBD
+mongoose
+    .connect(db,{ useNewUrlParser: true })
+    .then(()=>{console.log('connected succsess')})
+    .catch(()=>{console.log('error')});
+    
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`Server running on port ${port}`));
